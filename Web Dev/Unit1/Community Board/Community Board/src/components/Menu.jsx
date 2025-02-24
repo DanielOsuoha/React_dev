97e49e33cadd835abe7b208c73d996c439760bcd
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-02-23 20:44:21 -0600

    added the roof image

--- Web Dev/Unit1/Community Board/Community Board/src/components/Menu.jsx
+++ Web Dev/Unit1/Community Board/Community Board/src/components/Menu.jsx
@@ -4,6 +4,7 @@ import MenuItem from "./MenuItem";
 const Menu = () => {
     return (
         <div className="menu">
+            <img src="Web Dev/Unit1/Community Board/Community Board/src/assets/roof.png" alt="" />
             <h2>Food Truck Favourites</h2>
             <MenuItem />
             <MenuItem />
