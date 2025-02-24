68b883baafad1494e9c06cc610d906605a405e57
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-02-23 22:00:09 -0600

    added the images

--- Web Dev/Unit1/Community Board/Community Board/src/components/Menu.jsx
+++ Web Dev/Unit1/Community Board/Community Board/src/components/Menu.jsx
@@ -8,6 +8,7 @@ const Menu = () => {
             <img src={roofImage} alt="" />
             <h2>Food Truck Favourites</h2>
             <div className="menu-row">
+
                 <MenuItem name='Birria-Landia' location='Mexican'/>
                 <MenuItem name='The Halal Guys' location='Middle Eastern'/>
                 <MenuItem name='NY Dosas' location='Vegetarian'/>
@@ -18,9 +19,14 @@ const Menu = () => {
                 <MenuItem name='King Souvlaki of Astoria' location='Greek'/>
             </div>
             <div className="menu-row">
-                <MenuItem name="Ling's Sweet Mini Cakes" location='India'/>
-                <MenuItem name='Mysttik Masala' location='India'/>
-                <MenuItem name='Mysttik Masala' location='India'/>
+                <MenuItem name="Ling's Sweet Mini Cakes" location='Chinese'/>
+                <MenuItem name="Uncle Gussy's" location='Greek'/>
+                <MenuItem name='Patacon Pisao' location='Venezuelan'/>
+            </div>
+            <div className="menu-row">
+                <MenuItem name="Mom's Mono" location='Tibetan'/>
+                <MenuItem name="Makina Cafe" location='Ethiopian'/>
+                <MenuItem name='Mysttik Masala' location='Indian'/>
             </div>
         </div>
     );
