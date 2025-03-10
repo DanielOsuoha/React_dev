6349022b6a91b34e623e2930e78b0ae9b80e27a7
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-03-09 20:35:19 -0500

    completed ui

--- Web Dev/Unit2/Lab/samosa-selector/src/App.jsx
+++ Web Dev/Unit2/Lab/samosa-selector/src/App.jsx
@@ -5,10 +5,29 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
   function updateCount(){
+    alert('You clicked');
     setCount(count + multiplier);
   }
-  const [multiplier, setMultiplier] = useState(1);
+  function buyDoubleStuffed(){
+    if (count >= 10){
+      setCount(count - 10);
+      setMultiplier(multiplier * 2);
+    }
+  }
+  function buyPartyPack(){
+    if (count >= 100){
+      setCount(count - 100);
+      setMultiplier(multiplier * 5);
+    }
+  }
+  function buyFullFeast(){
+    if (count >= 1000){
+      setCount(count - 1000);
+      setMultiplier(multiplier * 10);
+    }
+  }
   return (
     <div className='App'>
       <h1>Sam's Samosa Selector</h1>
@@ -19,17 +38,17 @@ function App() {
         <div className='upgrade'>
           <h3>Double Stuffed 👯‍♀️	</h3>
           <p>2x per click	</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
         <div className='upgrade'>
           <h3>Party Pack 🎉	</h3>
           <p>5x per click	</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPack}>100 samosas</button>
         </div>
         <div className='upgrade'>
           <h3>Full Feast 👩🏽‍🍳	</h3>
           <p>10x per click	</p>
-          <button>100 samosas</button>
+          <button onClick={buyFullFeast}>100 samosas</button>
         </div>
       </div>
     </div>
